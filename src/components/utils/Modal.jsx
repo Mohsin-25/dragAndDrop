4466db005262460706839bad8bed56b340bd21a4
Mohsin-25 <mohsinmohammad122@gmail.com>
2025-04-26 15:32:07 +0530

    fix : backdrop changes

--- src/components/utils/Modal.jsx
+++ src/components/utils/Modal.jsx
@@ -16,12 +16,12 @@ export default function Modal({ children, title, trigger }) {
         className="relative z-10 focus:outline-none"
       >
         <div
-          className="fixed inset-0 bg-black/40 backdrop-blur-sm"
+          className="fixed inset-0 bg-black/40 backdrop-blur-xs backdrop-invert-25"
           aria-hidden="true"
         />
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4">
-            <div className="backdrop-blur-xs bg-black rounded-xl">
+            <div className="bg-black rounded-xl">
               <DialogPanel className="w-full min-w-[600px] rounded-xl bg-white/10 p-6 ">
                 <DialogTitle className=" text-2xl font-medium text-white text-center">
                   {title}
